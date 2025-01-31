36ccb5919629926c8306db032ca395669d574189
Laurence <laurenceunabia@gmail.com>
2025-01-31 14:05:43 +0800

    update

--- app.js
+++ app.js
@@ -42,11 +42,13 @@ app.use(mongoSanitize({replaceWith: '_'}));
 
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
